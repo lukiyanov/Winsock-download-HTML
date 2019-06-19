37ac985c2a7c3482d0d10a27aaf27e8b28a1513b
Alexey <lukyanov.aw@gmail.com>
2019-06-19 15:41:02 +0300

    Добавлены тесты и обработка командной строки

--- Ankard-task/Ankard-task/Ankard-task.cpp
+++ Ankard-task/Ankard-task/Ankard-task.cpp
@@ -1,6 +1,32 @@
 ﻿#include <iostream>
+#include "CmdLineParser.h"
 
+// ----------------------------------------------------------------------------
 int main(int argc, char** argv)
 {
+	using namespace task;
+	try
+	{
+		CmdLineParser cmdLine(argc, argv);
+
+		//CheckWriteAccessibility(cmdLine.GetDirectory());
+
+		//PageDownloader page(cmdLine.GetUrl());
+
+	}
+	catch (std::exception& ex)
+	{
+		std::cout << "An exception has been raised.\n" << ex.what() << std::endl;
+		return 1;
+	}
+	catch (...)
+	{
+		std::cout << "An unknown exception has been raised." << std::endl;
+		return 2;
+	}
+
+	std::cout << "Done." << std::endl;
 	return 0;
 }
+
+// ----------------------------------------------------------------------------
---
+++ Ankard-task/Ankard-task/CmdLineParser.cpp
@@ -0,0 +1,17 @@
+#include "CmdLineParser.h"
+#include "CmdLineExceptions.h"
+using namespace task;
+using namespace std;
+
+// ----------------------------------------------------------------------------
+CmdLineParser::CmdLineParser(int argc, char** argv)
+{
+	const int expectedArgCount = 2;
+	if (argc != expectedArgCount + 1)
+		throw CommandLineException("Invalid argument count (2 expected).");
+
+	m_url      = argv[1];
+	m_drectory = argv[2];
+}
+
+// ----------------------------------------------------------------------------
